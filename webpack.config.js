7160148f35b975366812d3b7f9b1d8dee51796e7
sunshine9266 <suming9266@163.com>
2017-11-07 11:25:23 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,11 @@ module.exports = {
         exclude: /node_modules/
       },
       {
+        test: /\.html$/,
+        loader: 'babeloader',
+        exclude: /node_modules/
+      },
+      {
         test: /\.(png|jpg|gif|svg)$/, 
         loader: 'file-loader',
         options: {
